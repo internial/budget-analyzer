a9e3ef1312994d2cc18e4254f7e25c124a96f159
Prabhat Shrestha <rockwirez@gmail.com>
2026-04-08 01:28:01 -0400

    Remove deprecated DynamoDB state lock and S3 ACL resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,11 +173,7 @@ resource "aws_s3_bucket" "uploads_replica" {
   })
 }
 
-resource "aws_s3_bucket_acl" "uploads_replica" {
-  provider = aws.replica
-  bucket   = aws_s3_bucket.uploads_replica.id
-  acl      = "private"
-}
+
 
 resource "aws_s3_bucket_versioning" "uploads_replica" {
   provider = aws.replica
@@ -1189,20 +1185,7 @@ resource "aws_budgets_budget" "monthly" {
   depends_on = [aws_sns_topic_policy.budget_alerts]
 }
 
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "budget-analyzer-terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = merge(local.common_tags, {
-    Name = "budget-analyzer-terraform-state-lock"
-  })
-}
 
 output "budget_analyzer_dlq_arn" {
   description = "The ARN of the budget analyzer SQS Dead Letter Queue"
